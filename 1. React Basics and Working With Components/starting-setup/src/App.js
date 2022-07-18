457a076ed82a36eaa85cb84f0c9b5bea946e2e9f
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-07-18 21:00:36 +0530

    Building a First Custom Component

--- 1. React Basics and Working With Components/starting-setup/src/App.js
+++ 1. React Basics and Working With Components/starting-setup/src/App.js
@@ -1,7 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem"; //importing our custom component
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      {/* using custom component as HTML tag */}
+      {/* look for naming convention as name starts with uppercase for custom components */}
     </div>
   );
 }
