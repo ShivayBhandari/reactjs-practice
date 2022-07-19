2c43412089ecba9a87b9a2747c39b2705e91e4ce
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-07-19 12:26:10 +0530

    Adding Basic CSS Styling

--- 1. React Basics and Working With Components/starting-setup/src/components/ExpenseItem.js
+++ 1. React Basics and Working With Components/starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
+import "./ExpenseItem.css"; //importing css file
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
+      {/* added className in the <div></div> */}
       <div>March 28th 2022</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>11000</div>
+        <div className="expense-item__price">11000</div>
       </div>
     </div> //one root element
   );
