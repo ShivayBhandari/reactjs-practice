eced5a9154f052435dbd9836c64019425cea8815
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-07-20 21:11:01 +0530

    Adding Normal JavaScript Logic to Components

--- 1. React Basics and Working With Components/starting-setup/src/components/ExpenseItem.js
+++ 1. React Basics and Working With Components/starting-setup/src/components/ExpenseItem.js
@@ -3,10 +3,18 @@ import "./ExpenseItem.css"; //importing css file
 function ExpenseItem(props) {
   //props parameter contains all the attributes that are defined in the App.js component ExpenseItem
 
+  const month = props.date.toLocaleString("en-US", { month: "long" }); //JavaScript function
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
       {/* added className in the <div></div> */}
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       {/* output date as string bcoz expenseDate is an object, it will not be displayed directly */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
