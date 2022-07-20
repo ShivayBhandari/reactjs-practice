f53b9ea71973c2f29db57d885b7e426551cc9051
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-07-20 20:40:44 +0530

    Passing Data via _props_

--- 1. React Basics and Working With Components/starting-setup/src/components/ExpenseItem.js
+++ 1. React Basics and Working With Components/starting-setup/src/components/ExpenseItem.js
@@ -1,22 +1,17 @@
 import "./ExpenseItem.css"; //importing css file
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 2, 28); //regular JavaScript
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 11000;
+function ExpenseItem(props) {
+  //props parameter contains all the attributes that are defined in the App.js component ExpenseItem
 
   return (
     <div className="expense-item">
       {/* added className in the <div></div> */}
-
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       {/* output date as string bcoz expenseDate is an object, it will not be displayed directly */}
-
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
         {/* {}used to output dynamic data */}
-
-        <div className="expense-item__price">{expenseAmount}</div>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div> //one root element
   );
