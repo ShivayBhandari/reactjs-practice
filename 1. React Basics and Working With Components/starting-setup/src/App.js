fbd4a19d549a3bb6bfc678d95a97a10e0c100ba2
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-07-20 21:56:59 +0530

    Splitting Components Into Multiple Components

--- 1. React Basics and Working With Components/starting-setup/src/App.js
+++ 1. React Basics and Working With Components/starting-setup/src/App.js
@@ -1,8 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem"; //importing our custom component
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
-    //expenses array for the props concept
     {
       id: "e1",
       title: "Toilet Paper",
@@ -27,27 +26,27 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      {/* passing the data as props (attributes) */}
+      {/* passing the data as (attributes) */}
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      ></ExpenseItem>
+      />
       {/* using custom component as HTML tag */}
       {/* look for naming convention as name starts with uppercase for custom components */}
     </div>
--- 1. React Basics and Working With Components/starting-setup/src/components/ExpenseItem.js
+++ 1. React Basics and Working With Components/starting-setup/src/components/ExpenseItem.js
@@ -1,27 +1,18 @@
-import "./ExpenseItem.css"; //importing css file
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  //props parameter contains all the attributes that are defined in the App.js component ExpenseItem
-
-  const month = props.date.toLocaleString("en-US", { month: "long" }); //JavaScript function
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      {/* added className in the <div></div> */}
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
-      {/* output date as string bcoz expenseDate is an object, it will not be displayed directly */}
+      <ExpenseDate date={props.date} />
+      {/* passing the data as (attributes) for ExpenseDate.js component */}
+
+      {/* same as <ExpenseDate></ExpenseDate> */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        {/* {}used to output dynamic data */}
         <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div> //one root element
+    </div>
   );
 }
 
